6324f3bb02b71b60d1d5d6b9ff231c9c235323cf
gaurav yadav <gauravopensource95@gmail.com>
2024-09-07 20:21:52 +0530

    Added .gitignore and untracked files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,18 +9,23 @@ import AboutPage from "./landing_page/about/AboutPage";
 import ProductPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
 import SupportPage from "./landing_page/support/SupportPage";
+//edkfhjkfhjk
 
 import NotFound from "./landing_page/NotFound";
 import Navbar from "./landing_page/Navbar";
 import Footer from "./landing_page/Footer";
+import Login from "./landing_page/signup/Login";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Navbar />
+  <Navbar/>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    
       <Route path="/about" element={<AboutPage />} />
       <Route path="/product" element={<ProductPage />} />
       <Route path="/pricing" element={<PricingPage />} />
